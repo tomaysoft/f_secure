ed6a91950e11bad890783e4216a6c8a1aed5c33b
Tomasz Szkudlarek <tomasz.szkudlarek@komputronik.pl>
2019-07-04 09:45:32 +0200

    app rename

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static("fSecure"));
+app.use(express.static("myApp"));
 app.get('/', function (req, res, next) {
     res.redirect('/');
 });
